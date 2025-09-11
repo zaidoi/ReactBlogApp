f183393502ae838448af22541f6897a37be2124b
zaidoi <zaidbadgujar7@gmail.com>
2025-09-11 20:20:38 +0530

    Authentication Part Completed

---
+++ blogApp/src/appwrite/auth.js
@@ -0,0 +1,46 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if(userAccount){
+        return this.login({email,password})
+      }else{
+        return userAccount
+    }
+  }
+
+  async login({email,password}){
+        return await this.account.createEmailPasswordSession(email,password)
+  }
+
+  async logout(){
+    try {
+        await this.account.deleteSessions();
+    } catch (error) {
+        console.log("Appwrite service error:",error);
+        
+    }
+  }
+
+}
+
+const authService = new AuthService();
+
+export default authService;
