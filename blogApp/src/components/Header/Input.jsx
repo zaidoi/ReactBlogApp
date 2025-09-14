95fcf30baccbbc4ad5b6e73e04b1e2469f59b690
zaidoi <zaidbadgujar7@gmail.com>
2025-09-14 17:55:07 +0530

    Input element Select Made

---
+++ blogApp/src/components/Header/Input.jsx
@@ -0,0 +1,28 @@
+import { useId } from "react";
+import React from "react";
+
+const Input = React.forwardRef(function Input(
+  { label, type = "text", className = "", ...props },
+  ref
+) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="inline-block mb-1" htmlFor={id}>
+          {label}
+        </label>
+      )}
+
+      <input
+        type={type}
+        className={`${className} ox-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full `}
+        ref={ref}
+        {...props}
+        id={id}
+      />
+    </div>
+  );
+});
+
+export default Input;
